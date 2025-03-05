8b9c34949b919ff374064ed11a032c548bb4648c
ansdudn2 <ansdudn2@naver.com>
2025-03-05 11:14:51 +0900

    store create

--- src/main/java/com/example/deliveryapp/store/dto/response/StoreResponseDto.java
+++ src/main/java/com/example/deliveryapp/store/dto/response/StoreResponseDto.java
@@ -1,4 +1,38 @@
 package com.example.deliveryapp.store.dto.response;
 
+import com.example.deliveryapp.store.entity.Store;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoreResponseDto {
+
+    private Long id;
+    private String businessName;
+    private String category;
+    private LocalDateTime openingTime;
+    private LocalDateTime closingTime;
+    private Double minOrderPrice;
+    private String ownerUsername;  // 소유자의 username
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    //store 객체를 StoreResponseDto로변환
+    public StoreResponseDto(Store store) {
+        this.id = store.getId();
+        this.businessName = store.getBusinessName();
+        this.category = store.getCategory();
+        this.openingTime = store.getOpeningTime();
+        this.closingTime = store.getClosingTime();
+        this.minOrderPrice = store.getMinOrderPrice();
+        this.ownerUsername = store.getOwner().getUserName(); // 소유자의 username
+        this.createdAt = store.getCreatedAt();
+        this.updatedAt = store.getUpdatedAt();
+    }
 }
--- src/main/java/com/example/deliveryapp/store/entity/Store.java
+++ src/main/java/com/example/deliveryapp/store/entity/Store.java
@@ -1,4 +1,39 @@
 package com.example.deliveryapp.store.entity;
 
-public class Store {
+import com.example.deliveryapp.common.entity.BaseEntity;
+import com.example.deliveryapp.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Store extends BaseEntity {  // BaseEntity 상속
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String businessName;
+
+    private String category;
+
+    private LocalDateTime openingTime;
+
+    private LocalDateTime closingTime;
+
+
+    private Double minOrderPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;  // 가게의 주인(사장님)
+
 }
