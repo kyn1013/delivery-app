7a10405224e2bc84f4c7df73b006723775c8d307
ansdudn2 <ansdudn2@naver.com>
2025-03-06 09:18:53 +0900

    chor1

--- src/main/java/com/example/deliveryapp/auth/common/util/FilterConfig.java
+++ src/main/java/com/example/deliveryapp/auth/common/util/FilterConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @RequiredArgsConstructor
